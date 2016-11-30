8fe3c2744849249998d66b5d470a40197a1fba15
linux_china <linux_china@hotmail.com>
2016-11-30 14:21:40 +0800

    add @EnableBinding for Sink and Source

--- src/main/java/org/mvnsearch/cloud/CloudStreamDemoApplication.java
+++ src/main/java/org/mvnsearch/cloud/CloudStreamDemoApplication.java
@@ -2,8 +2,12 @@ package org.mvnsearch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableBinding({Sink.class, Source.class})
 public class CloudStreamDemoApplication {
 
 	public static void main(String[] args) {
