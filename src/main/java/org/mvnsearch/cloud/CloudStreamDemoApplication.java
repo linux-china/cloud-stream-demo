5447c615a476687d1be7e4af0723c12a6d630f51
linux_china <linux_china@hotmail.com>
2016-11-30 14:17:24 +0800

    cloud stream demo application

---
+++ src/main/java/org/mvnsearch/cloud/CloudStreamDemoApplication.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStreamDemoApplication.class, args);
+	}
+}
