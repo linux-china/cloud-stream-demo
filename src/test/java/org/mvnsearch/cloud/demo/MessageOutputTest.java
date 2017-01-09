dec12fdb88d292810c129dc5c008c7299eadbf0a
linux_china <linux_china@hotmail.com>
2017-01-09 21:09:44 +0800

    moved

--- src/test/java/org/mvnsearch/cloud/demo/MessageOutputTest.java
+++ src/test/java/org/mvnsearch/cloud/demo/MessageOutputTest.java
@@ -1,11 +1,13 @@
-package org.mvnsearch.cloud;
+package org.mvnsearch.cloud.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mvnsearch.cloud.CloudStreamDemoApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -14,13 +16,14 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author linux_china
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,classes = CloudStreamDemoApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class MessageOutputTest {
     @Autowired
     private Source source;
 
     @Test
     public void testSend() {
-        source.output().send(MessageBuilder.withPayload("good").build());
+        Message<String> message = MessageBuilder.withPayload("Good Afternoon").build();
+        source.output().send(message);
     }
 }
